aad5c9ebda8109f8be8e47f66c523b2082248793
VitaliyTheKeyboardist <vital10@inbox.ru>
2024-03-24 23:48:22 +0300

    page 404

--- t1projectvite/src/App.tsx
+++ t1projectvite/src/App.tsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import MainPage from "./components/Pages/MainPage/MainPage"
 import Blog from "./components/Pages/Blog/Blog"
 import OneArticle from "./components/Organisms/OneArticle/OneArticle"
-import "./App.css"
 import MainLayout from "./Layouts/MainLayout"
+import Page404 from "./components/Pages/Page404/Page404"
+import "./App.css"
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
             <Route index element={<MainPage />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/article" element={<OneArticle />} />
-            <Route path="*" element={<h1>Not Found</h1>} />
+            <Route path="*" element={<Page404 />} />
           </Route>
         </Routes>
       </BrowserRouter>
